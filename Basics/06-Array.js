5d8b5b25eabbdd8cb1866c575b7a7974328a5d74
sachin <sk800968@gmail.com>
2024-09-11 16:31:03 +0530

    Array

---
+++ Basics/06-Array.js
@@ -0,0 +1,16 @@
+// VariableName : type[] = []   //syntex
+var superHeros1 = [];
+var heroId = [];
+var allUsers1 = [];
+superHeros1.push("spiderman");
+heroId.push(2);
+allUsers1.push({ name: "", isActive: true });
+//output
+console.log(superHeros1);
+console.log(heroId);
+console.log(allUsers1);
+//2D Array
+var MLModels = [
+    [255, 255, 255],
+    []
+];
---
+++ Basics/06-Array.ts
@@ -0,0 +1,37 @@
+
+// VariableName : type[] = []   //syntex
+const superHeros1: string[] = []
+
+const heroId: number[] = []
+// const heroPower2: Array<number> = []
+
+type User1 = {
+    name: string
+    isActive: boolean
+}
+
+const allUsers1: User1[] = []
+
+
+superHeros1.push("spiderman")
+heroId.push(2)
+
+allUsers1.push({name: "", isActive: true})
+
+
+
+
+//output
+console.log(superHeros1);
+console.log(heroId);
+console.log(allUsers1);
+
+
+
+//2D Array
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+]
+
+
